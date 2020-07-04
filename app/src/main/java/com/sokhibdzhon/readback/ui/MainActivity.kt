a2137f36523f98af86bb20523f8fa9d104aa205b
Sokhibdzhon <sokhibsaid@gmail.com>
2020-07-04 10:52:23 +0300

    NavGraph added App colors added TextColor style added

--- app/src/main/java/com/sokhibdzhon/readback/ui/MainActivity.kt
+++ app/src/main/java/com/sokhibdzhon/readback/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sokhibdzhon.readback
+package com.sokhibdzhon.readback.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sokhibdzhon.readback.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sokhibdzhon/readback/ui/start/StartFragment.kt
@@ -0,0 +1,21 @@
+package com.sokhibdzhon.readback.ui.start
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sokhibdzhon.readback.R
+
+class StartFragment : Fragment() {
+
+    private lateinit var viewModel: StartViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.start_fragment, container, false)
+    }
+
+}
