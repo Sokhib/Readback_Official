a480a627f8c5ee8f00b7813eb617d76557b6303b
Sokhibdzhon <sokhibsaid@gmail.com>
2020-07-06 15:26:02 +0300

    navigate to game fragment added

---
+++ app/src/main/java/com/sokhibdzhon/readback/ui/game/GameFragment.kt
@@ -0,0 +1,28 @@
+package com.sokhibdzhon.readback.ui.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.sokhibdzhon.readback.R
+
+class GameFragment : Fragment() {
+
+    private lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.game_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+
+    }
+
+}
--- app/src/main/java/com/sokhibdzhon/readback/ui/start/StartFragment.kt
+++ app/src/main/java/com/sokhibdzhon/readback/ui/start/StartFragment.kt
@@ -4,18 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sokhibdzhon.readback.R
+import com.sokhibdzhon.readback.databinding.StartFragmentBinding
 
 class StartFragment : Fragment() {
 
+    private lateinit var binding: StartFragmentBinding
     private lateinit var viewModel: StartViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.start_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.start_fragment, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.startGame.setOnClickListener {
+            navigateToGame()
+        }
+    }
+
+    private fun navigateToGame() {
+        val direction = StartFragmentDirections.actionStartFragmentToGameFragment()
+        this.findNavController().navigate(direction)
+    }
 }
