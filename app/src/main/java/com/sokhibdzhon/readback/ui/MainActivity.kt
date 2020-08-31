01437c851c9d8b3cc57df9c113c4f97754fdb4cd
Sokhibdzhon <sokhibsaid@gmail.com>
2020-08-31 12:03:28 +0300

    READ-300 #update #comment Device added as Test device.

--- app/src/main/java/com/sokhibdzhon/readback/ui/MainActivity.kt
+++ app/src/main/java/com/sokhibdzhon/readback/ui/MainActivity.kt
@@ -3,12 +3,17 @@ package com.sokhibdzhon.readback.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.sokhibdzhon.readback.R
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         MobileAds.initialize(this)
+        val testDeviceIds = Arrays.asList("7D8CC41A519581A5551D52F115EA2F2B")
+        val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
+        MobileAds.setRequestConfiguration(configuration)
     }
 }
