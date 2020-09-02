93529442483285a8b283ec09f64744133afd6f2c
Sokhibdzhon <sokhibsaid@gmail.com>
2020-09-02 21:15:08 +0300

    READ-300 #comment minik oyunlar

--- app/src/main/java/com/sokhibdzhon/readback/ui/MainActivity.kt
+++ app/src/main/java/com/sokhibdzhon/readback/ui/MainActivity.kt
@@ -5,14 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration
 import com.sokhibdzhon.readback.R
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         MobileAds.initialize(this)
-        val testDeviceIds = Arrays.asList("7D8CC41A519581A5551D52F115EA2F2B")
+        val testDeviceIds = listOf("7D8CC41A519581A5551D52F115EA2F2B")
         val configuration = RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build()
         MobileAds.setRequestConfiguration(configuration)
     }
